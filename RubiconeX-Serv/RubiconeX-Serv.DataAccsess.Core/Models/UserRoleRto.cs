21c308748a603be4e1545c2ed9d52e6e453fe079
stewe15 <65032096+stewe15@users.noreply.github.com>
2021-12-04 16:36:35 +0300

    Add files via upload

---
+++ RubiconeX-Serv/RubiconeX-Serv.DataAccsess.Core/Models/UserRoleRto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubiconeX_Serv.DataAccsess.Core.Models
+{
+    class UserRoleRto
+    {
+    }
+}
---
+++ RubiconeX-Serv/RubiconeX-Serv.DataAccsess.Core/Models/UserRto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RubiconeX_Serv.DataAccsess.Core.Models
+{
+    [Table("User")]
+    public class UserRto
+    {
+        [Key] public int Id { get; set; }
+        public int PhoneNumberPreFix { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronumic { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Introdution { get; set; }
+        public UserRoleRto Role { get; set; }
+
+
+
+
+        [NotMapped]
+        public string GetFullName
+        {
+            get => LastName + " " + FirstName + " " + Patronumic;
+        }
+    }
+}
+}
